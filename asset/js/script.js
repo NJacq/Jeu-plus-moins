beb13d543b3e65f2779be8de3af1b9a1a603431c
NJacq <nicolas.j@codeur.online>
2018-03-12 19:15:30 +0100

    maj2

--- asset/js/script.js
+++ asset/js/script.js
@@ -25,14 +25,16 @@ var nb_random = Math.floor(Math.random()*100)+1;
      console.log(nb_random)
 var bouton = document.getElementById('bouton');
 var coup = 0
-var max=100
+var max = 100
+var min = 1
 bouton.addEventListener('click', function()
 
 {
 var saisie = document.getElementById('proposition').value;
 coup++
-if (saisie>max){
-    document.getElementById('resultat').innerHTML = "Ce nombre est trop important"
+if ((saisie>100)||(saisie<1)){
+    document.getElementById('resultat').innerHTML = "Ce nombre est incorrect : il doit être compris entre 1 et 100!"
+
 }
 else{
 
@@ -40,10 +42,10 @@ if (saisie==nb_random){
     document.getElementById('resultat').innerHTML = "C'est la bonne réponse. Vous avez eu besoin de "+coup+" tentavives.";
 }
 if (saisie<nb_random){
-    document.getElementById('resultat').innerHTML = "C'est plus !";
+    document.getElementById('resultat').innerHTML = "C'est plus!";
 }
 if (saisie>nb_random){
-    document.getElementById('resultat').innerHTML = "C'est moins !";
+    document.getElementById('resultat').innerHTML = "C'est moins!";
 }
 }
 
